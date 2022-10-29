0ca40c54673d1cebb29083fd8b9dd9c449c2635d
egg <ljjain0508@gmail.com>
2022-10-29 14:04:52 +0800

    create room

--- README.md
+++ README.md
@@ -6,10 +6,21 @@
 ## 日誌
 > 2022.10.29
 - started Project (studybud) & App (base)
-
+- urls & templates 
+- database & createsuperuser
 
 ## 筆記
-> 
+> Models
+- auto_now_add 只會新增第一次的時間 / auto_now 只要有修改就會變更時間
+- 每次修改就要做一次 makemigrations
+- 導入models:
+
+    from .models import Room
+
+    admin.site.register(Room)
+- views:
+
+    from .models import Room
 
 ## 課程網站
 > https://www.youtube.com/watch?v=PtQiiknWUcI&t=297s&ab_channel=TraversyMedia
--- base/views.py
+++ base/views.py
@@ -1,22 +1,29 @@
 from django.shortcuts import render
 
+from .models import Room
+
 # Create your views here.
 
-rooms = [
-    {'id':1, 'name':'中文假文產生器'},
-    {'id':2, 'name':'這是什麼鬼東西？'},
-    {'id':3, 'name':'這能幹嘛？'},
-]
+# rooms = [
+#     {'id':1, 'name':'中文假文產生器'},
+#     {'id':2, 'name':'這是什麼鬼東西？'},
+#     {'id':3, 'name':'這能幹嘛？'},
+# ]
 
 
 def home(request):
+    rooms = Room.objects.all()
+
     context = {'rooms':rooms}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    room = Room.objects.get(id=pk)
+
     context = {'room': room}
     return render(request, 'base/room.html', context)
+
+def createRoom(request):
+
+    context = {}
+    return render(request, 'base/room_form.html', context)
