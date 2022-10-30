e3a60d71efc5491f97cb88c94a91f44dea5f6a0d
Eggsy <ljjain0508@gmail.com>
2022-10-30 16:09:15 +0800

    static

--- base/urls.py
+++ base/urls.py
@@ -8,8 +8,11 @@ urlpatterns = [
     path('login/', views.loginPage, name='login'),
     path('logout/', views.logoutUser, name='logout'),
     path('register/', views.registerPage, name='register'),
+    path('profile/<str:pk>/', views.profilePage, name='profile'),
 
     path('create_room', views.createRoom, name='create_room'),
     path('update_room/<str:pk>/', views.updateRoom, name='update_room'),
     path('delete_room/<str:pk>/', views.deleteRoom, name='delete_room'),
+
+    path('delete_message/<str:pk>/', views.deleteMessage, name='delete_message'),
 ]    
--- base/views.py
+++ base/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import  login_required
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.db.models import Q
 
-from .models import Room, Topic
+from .models import Room, Topic, Message
 from .forms import RoomForm
 # Create your views here.
 
@@ -48,8 +49,20 @@ def logoutUser(request):
     return redirect('home')
 
 def registerPage(request):
-    page='register'
-    context = {'page':page}
+    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user=form.save(commit=False)
+            user.username=user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'An error occurred during registration!!!')
+
+
+    context = {'form':form}
     return render(request, 'base/login_register.html', context)
 
 def home(request):
@@ -62,14 +75,40 @@ def home(request):
     topics = Topic.objects.all()
     room_count = rooms.count()
 
-    context = {'rooms':rooms, 'topics':topics, 'room_count':room_count}
+    room_messages=Message.objects.filter(Q(room__topic__name__icontains=q))
+
+
+    context = {'rooms':rooms, 'topics':topics, 'room_count':room_count, 'room_messages':room_messages}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
+    room_messages=room.message_set.all()
+    participants=room.participants.all()
 
-    context = {'room': room}
+    if request.method == 'POST':
+        message=Message.objects.create(
+            user=request.user,
+            room=room,
+            body=request.POST.get('body'),
+        )
+        room.participants.add(request.user)
+        return redirect('room', pk=room.id)
+
+
+    context = {'room': room, 'room_messages':room_messages, 'participants':participants}
     return render(request, 'base/room.html', context)
+
+def profilePage(request, pk):
+    user=User.objects.get(id=pk)
+    rooms=user.room_set.all()
+    room_messages=user.message_set.all()
+    topics=Topic.objects.all()
+
+
+
+    context = {'user': user, 'rooms':rooms, 'room_messages':room_messages, 'topics':topics}
+    return render(request, 'base/profile.html', context)
 
 @login_required(login_url='login')
 def createRoom(request):
@@ -114,4 +153,18 @@ def deleteRoom(request, pk):
         return redirect('home')
 
     context = {'obj':room}
+    return render(request, 'base/delete.html', context)
+
+@login_required(login_url='login')
+def deleteMessage(request, pk):
+    message = Message.objects.get(id=pk)
+
+    if request.user != message.user:
+        return HttpResponse('You are not allowed to delete !!!')
+
+    if request.method == 'POST':
+        message.delete()
+        return redirect('home')
+
+    context = {'obj':message}
     return render(request, 'base/delete.html', context)
