f5dc175af104a8521c2d2001fe3267159822ba82
Eggsy <ljjain0508@gmail.com>
2022-10-30 23:58:22 +0800

    Theme

--- base/views.py
+++ base/views.py
@@ -1,3 +1,4 @@
+from pydoc_data.topics import topics
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User
@@ -109,34 +110,48 @@ def profilePage(request, pk):
 @login_required(login_url='login')
 def createRoom(request):
     form  = RoomForm()
-
+    topics=Topic.objects.all()
     if request.method == 'POST':
-        form = RoomForm(request.POST)
-        if form.is_valid():
-            room=form.save(commit=False)
-            room.host=request.user
-            room.save()
-            return redirect('home')
+        topic_name = request.POST.get('topic')
+        topic, created = Topic.objects.get_or_create(name=topic_name)
+        
+        Room.objects.create(
+            host=request.user,
+            topic=topic,
+            name=request.POST.get('name'),
+            description=request.POST.get('description'),
+            )
+        return redirect('home')
+        # form = RoomForm(request.POST)
+        # if form.is_valid():
+        #     room=form.save(commit=False)
+        #     room.host=request.user
+        #     room.save()
+            
 
-    context = {'form': form}
+    context = {'form': form, 'topics':topics}
     return render(request, 'base/room_form.html', context)
 
 @login_required(login_url='login')
 def updateRoom(request, pk):
     room = Room.objects.get(id=pk)
     form = RoomForm(instance=room)  # instance:預先填入內容
-
+    topics=Topic.objects.all()
     if request.user != room.host:
         return HttpResponse('You are not allowed to update !!!')
 
 
     if request.method == 'POST':
-        form = RoomForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('home')
+        topic_name = request.POST.get('topic')
+        topic, created = Topic.objects.get_or_create(name=topic_name)
 
-    context = {'form': form, }
+        room.name=request.POST.get('name')
+        room.topic=topic
+        room.description=request.POST.get('description')
+        room.save()
+        return redirect('home')
+
+    context = {'form': form, 'topics': topics, 'room': room}
     return render(request, 'base/room_form.html', context)
 
 @login_required(login_url='login')
